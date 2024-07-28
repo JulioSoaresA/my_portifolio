7f180d187df7de98b8f6868a6e618379e764b030
Júlio Soares <julio.soares@lais.huol.ufrn.br>
2024-07-28 17:09:53 -0300

    feat: animaçoes

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const myObservert = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+            }
+        });
+    });
+
+    const elements = document.querySelectorAll(".hidden");
+
+    elements.forEach((element) => myObservert.observe(element));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
     let menuIcon = document.querySelector("#menu-icon");
     let navbar = document.querySelector(".navbar");
 
@@ -6,4 +22,27 @@ document.addEventListener("DOMContentLoaded", function () {
         menuIcon.classList.toggle("bx-x");
         navbar.classList.toggle("active");
     };
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var fullText = document.getElementById("about-text").innerHTML;
+    var truncatedText = fullText.substring(0, 200) + "...";
+    var isTruncated = true;
+    var btn = document.getElementById("read-more-btn");
+    var paragraph = document.getElementById("about-text");
+
+    // Initially show truncated text
+    paragraph.innerHTML = truncatedText;
+
+    btn.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (isTruncated) {
+            paragraph.innerHTML = fullText;
+            btn.textContent = "Leia Menos";
+        } else {
+            paragraph.innerHTML = truncatedText;
+            btn.textContent = "Leia Mais";
+        }
+        isTruncated = !isTruncated;
+    });
 });
